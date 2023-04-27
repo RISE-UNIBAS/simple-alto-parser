bd0116deca29ac95e76176f037798b641f02803f
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2023-04-27 16:52:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from setuptools import setup
 import os
 
-with open(os.path.join(Path(__file__).resolve().parent, 'ndr_core', 'VERSION')) as version_file:
+with open(os.path.join(Path(__file__).resolve().parent, 'VERSION')) as version_file:
     version = version_file.read().strip()
 
 setup(version=version)
