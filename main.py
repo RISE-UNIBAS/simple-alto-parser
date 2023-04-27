d72bfe75821e89e1d25b7880c133a55e8477deb3
Sorin Marti <32014438+sorinmarti@users.noreply.github.com>
2023-04-27 16:47:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from updated_alto_parser import AltoTextParser
 
 alto_parser = AltoTextParser()
-alto_parser.add_files('data_1923')
-"""alto_parser.add_file('data_1923_orig/1923_0021.xml')"""
+alto_parser.add_files('assets/data')
 alto_parser.extract_meta_from_filenames('year', r'^(\d{4})')
 alto_parser.extract_meta_from_filenames('page', r'_(\d{4})')
 alto_parser.add_meta_data_to_files('title', 'Some title')
@@ -11,15 +10,11 @@ alto_parser.parse_text()
 alto_parser.export_lines_to_csv('data_1923.csv')
 alto_parser.export_lines_to_json('data_1923.json')
 
-alto_parser.export_regions_to_csv('data_1923r.csv')
-alto_parser.export_regions_to_json('data_1923r.json')
-
-
 result = alto_parser.get_alto_files()
 
 for file in result:
-    print(file.file_meta_data)
-    """lines = file.get_text_lines()
+    print("Meta data", file.file_meta_data)
+    lines = file.get_text_lines()
     for line in lines:
-        print(line)"""
+        print("Line", line)
 
